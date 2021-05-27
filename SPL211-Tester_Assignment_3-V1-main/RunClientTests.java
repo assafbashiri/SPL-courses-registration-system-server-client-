52b980b5ef2d0ba094538681495fcf950c77ea7d
assafbashiri <71321725+assafbashiri@users.noreply.github.com>
2021-05-27 12:37:56 +0300

    Delete SPL211-Tester_Assignment_3-V1-main directory

--- SPL211-Tester_Assignment_3-V1-main/RunClientTests.java
+++
@@ -1,9 +0,0 @@
-package bgu.spl.net.impl.BGRSServer.Tester;
-
-
-public class RunClientTests {
-
-        public static void main(String[] args) {
-                new Thread(new Tests()).start();
-        }
-}
